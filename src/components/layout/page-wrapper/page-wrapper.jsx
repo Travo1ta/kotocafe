9dcd42208b3a04da3bccb0a52a2425498ffda4aa
Vladimir <104868739+Travo1ta@users.noreply.github.com>
2025-12-18 14:14:02 +0300

    configured file paths

--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header/header";
-import Footer from "../footer/footer";  // ← исправлено
+import Footer from "../footer/footer"; 
 import MainPage from "../../pages/main-page";
 import "./style.css";
 
