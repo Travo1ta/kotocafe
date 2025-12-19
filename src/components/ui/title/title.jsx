828ae863ee6260c9fa2bfc0857aeba66b48cdfec
Vladimir <104868739+Travo1ta@users.noreply.github.com>
2025-12-19 15:27:35 +0300

    configured and wrote the file About

---
+++ src/components/ui/title/title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css';
+import { TitleSize } from './constants';  // ← импорт из constants.js
+
+function Title({ children, size = TitleSize.DEFAULT }) {
+    const Tag = size === TitleSize.BIG ? 'h1' : 
+                size === TitleSize.SMALL ? 'h3' : 'h2';
+    
+    return (
+        <Tag className={`title title--${size}`}>
+            {children}
+        </Tag>
+    );
+}
+
+export default Title;
