ec4b592d4f236bcf2e7152a96acda2f2741abbf0
Travo1ta <vladimirkobec@gmail.com>
2026-02-02 14:22:51 +0300

    Styling elements

---
+++ src/styled/index.js
@@ -0,0 +1,3 @@
+export { default as StyledSection } from './section/section.jsx';
+export { default as StyledUl } from './ul/ul.jsx';
+export { default as StyledLi } from './li/li.jsx';
---
+++ src/styled/section/section.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+  margin: 0;
+  padding: 0;
+`;
+
+export default StyledSection;
