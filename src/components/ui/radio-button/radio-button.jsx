aaf91093d40703d2c015b5b56f76984bdf17df0c
Travo1ta <vladimirkobec@gmail.com>
2026-02-23 16:22:12 +0300

    Created a RadioButton component in the ui directory

---
+++ src/components/ui/radio-button/radio-button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import VisuallyHiddenInput from '../../../styled/visually-hidden-input/visually-hidden-input';
+
+const Label = styled.label`
+  display: block;
+`;
+
+// Радиокнопка
+function RadioButton({
+   labelComponent: LabelComponent, // компонент для отображения label
+   selectValue, // выбранное значение
+   name, // имя
+   value, // значение
+   text, // текст элемента
+   onChange, // событие при изменении
+   ...props
+}) {
+   const isChecked = value === selectValue;
+
+   return (
+      <Label>
+         <VisuallyHiddenInput
+            value={value}
+            checked={isChecked}
+            name={name}
+            onChange={onChange}
+            {...props}
+            type="radio"
+         />
+         <LabelComponent $isChecked={isChecked}>{text}</LabelComponent>
+      </Label>
+   );
+}
+
+export default RadioButton;
